4909f7a6a6a79037af282fd35f40078e2c58ce1a
sj70 <shubhamjangid70@gmail.com>
2018-04-01 18:40:57 +0530

    added get/user/me and authenticate

--- server/models/User.js
+++ server/models/User.js
@@ -47,10 +47,25 @@ UserSchema.methods.generateAuthToken = function() {
   var token = jwt
     .sign({ _id: user._id.toHexString(), access }, "abc123")
     .toString();
-
   user.tokens = user.tokens.concat([{ access, token }]);
   return user.save().then(() => {
     return token;
+  });
+};
+
+UserSchema.statics.findByToken = function(token) {
+  var User = this;
+  var decoded;
+
+  try {
+    decoded = jwt.verify(token, "abc123");
+  } catch (e) {
+    return Promise.reject();
+  }
+  return User.findOne({
+    _id: decoded._id,
+    "tokens.token": token,
+    "tokens.access": "auth"
   });
 };
 
--- server/server.js
+++ server/server.js
@@ -6,6 +6,7 @@ const _ = require("lodash");
 var { mongoose } = require("./db/mongoose");
 var { Todo } = require("./models/Todo.js");
 var { User } = require("./models/User.js");
+var { authenticate } = require("./middleware/authenticate");
 
 var app = express();
 
@@ -99,26 +100,9 @@ app.patch("/todos/:id", (req, res) => {
     });
 });
 // signup user
-// app.post("/users", (req, res) => {
-//   var body = _.pick(req.body, ["email", "password"]);
-//   var user = new User(body);
-//   user
-//     .save()
-//     .then(() => {
-//       return user.generateAuthToken();
-//     })
-//     .then(token => {
-//       res.header("x-auth", token).send(user);
-//     })
-//     .catch(e => {
-//       res.status(400).send(e);
-//     });
-// });
-
 app.post("/users", (req, res) => {
   var body = _.pick(req.body, ["email", "password"]);
   var user = new User(body);
-
   user
     .save()
     .then(() => {
@@ -130,6 +114,12 @@ app.post("/users", (req, res) => {
     .catch(e => {
       res.status(400).send(e);
     });
+});
+
+// signing the user
+
+app.get("/users/me", authenticate, (req, res) => {
+  res.send(req.user);
 });
 
 // listening on port 3000
